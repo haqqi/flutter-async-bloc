c0d012b7548bdc1f1f93c7e084b8c23aef817e24
haqqi <me@haqqi.net>
2020-04-20 00:12:29 +0700

    👩 [ADD] state class

--- lib/flutter_async_bloc.dart
+++ lib/flutter_async_bloc.dart
@@ -1,3 +1,4 @@
 library flutter_async_bloc;
 
 export './src/core/result.dart';
+export './src/core/state.dart';
---
+++ lib/src/core/state.dart
@@ -0,0 +1,65 @@
+import 'package:equatable/equatable.dart';
+
+import '../../flutter_async_bloc.dart';
+
+/*********************************************/
+/************ Async state bloc ***************/
+/*********************************************/
+
+/// Base async state
+///
+abstract class AsyncState extends Equatable {
+  const AsyncState();
+
+  @override
+  List<Object> get props => [
+        runtimeType,
+      ];
+}
+
+/// Async initial state
+///
+class AsyncInitState extends AsyncState {
+  const AsyncInitState();
+}
+
+/// Async sending state
+///
+class AsyncSendingState extends AsyncState {
+  const AsyncSendingState();
+}
+
+/// Async done state
+///
+class AsyncDoneState extends AsyncState {
+  const AsyncDoneState();
+}
+
+/// Async success state with data of [D]
+///
+class AsyncSuccessState<D> extends AsyncState {
+  final D data;
+
+  AsyncSuccessState(this.data);
+
+  @override
+  List<Object> get props => [
+        data,
+      ];
+}
+
+/// Async error state with error message of [AsyncError]
+class AsyncErrorState extends AsyncState {
+  final AsyncError error;
+
+  AsyncErrorState(this.error);
+
+  @override
+  List<Object> get props => [
+        error,
+      ];
+}
+
+/*********************************************/
+/********* End of async state ****************/
+/*********************************************/
