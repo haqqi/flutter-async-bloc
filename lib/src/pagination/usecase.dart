3c21e575ff86d20226b4619325f027ad9e99c8a1
haqqi <me@haqqi.net>
2020-04-21 03:34:07 +0700

    🧧 [ADD] meta in usecase

--- lib/src/pagination/usecase.dart
+++ lib/src/pagination/usecase.dart
@@ -1,9 +1,13 @@
 import '../common/usecase.dart';
+import 'meta.dart';
 import 'response.dart';
 
 /// Unlimited list use case interface
 abstract class PaginationRequestUseCase<M>
     extends AsyncUseCaseInterface<PaginationAsyncResponse<M>> {
+  // pagination request meta with initial null value
+  PaginationRequestMeta meta;
+
   @override
   Future<PaginationAsyncResponse<M>> send();
 }
