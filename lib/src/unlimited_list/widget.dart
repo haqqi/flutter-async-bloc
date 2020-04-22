89c1d4adb1b7da69ec9beceda39378d7b072ba3c
haqqi <me@haqqi.net>
2020-04-23 00:59:47 +0700

    🐽 [ADD] options to display status

--- lib/src/unlimited_list/widget.dart
+++ lib/src/unlimited_list/widget.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
+import '../common/response.dart';
 import '../common/widget.dart';
 import 'bloc.dart';
 import 'event.dart';
@@ -14,14 +15,36 @@ class UnlimitedListWidget<M, U extends ULUseCase<M>> extends StatelessWidget {
   /// List tile builder that has parameter of [M]
   final ListTileBuilder<M> listTileBuilder;
 
+  /// Indexed separator widget
+  final IndexedWidgetBuilder separatorBuilder;
+
+  /// First loading widget builder. If it is null, default widget will be shown.
+  final WidgetBuilder loadingBuilder;
+
+  /// First loading error widget builder. If it is null, error message will be shown.
+  final Widget Function(BuildContext context, AsyncError error)
+      errorResultBuilder;
+
+  /// Empty message builder for first load
+  final WidgetBuilder emptyMessageBuilder;
+
+  /// Next page loading indicator
+  final Widget nextPageFetchingIndicator;
+
   /// Padding for the list view
   final EdgeInsetsGeometry padding;
 
   UnlimitedListWidget({
     @required this.listTileBuilder,
+    @required this.separatorBuilder,
+    this.loadingBuilder,
+    this.errorResultBuilder,
+    this.emptyMessageBuilder,
+    this.nextPageFetchingIndicator,
     this.padding,
     Key key,
   })  : assert(listTileBuilder != null),
+        assert(separatorBuilder != null),
         super(key: key);
 
   @override
@@ -38,17 +61,22 @@ class UnlimitedListWidget<M, U extends ULUseCase<M>> extends StatelessWidget {
           // if it is first fetching, show the on loading widget
           if (state.isFetching) {
             // just return the on loading widget
-            return OnLoadingWidget();
+            return OnLoadingWidget(
+              loadingBuilder: loadingBuilder,
+            );
           }
 
           if (state.hasError) {
             // if it has error
             effectiveWidget = OnErrorWidget(
+              errorBuilder: errorResultBuilder,
               error: state.error,
             );
           } else {
             // if the state is already success, but with zero data, show empty message
-            effectiveWidget = EmptyListMessageWidget();
+            effectiveWidget = EmptyListMessageWidget(
+              emptyListMessageBuilder: emptyMessageBuilder,
+            );
           }
         } else {
           // if the result is above 0, just display the result
@@ -59,7 +87,7 @@ class UnlimitedListWidget<M, U extends ULUseCase<M>> extends StatelessWidget {
             itemBuilder: (BuildContext context, int index) {
               // build loading process
               if (index == state.data.length) {
-                return LinearProgressIndicator();
+                return nextPageFetchingIndicator ?? LinearProgressIndicator();
               }
 
               // get the data
@@ -68,9 +96,7 @@ class UnlimitedListWidget<M, U extends ULUseCase<M>> extends StatelessWidget {
               // build it!
               return listTileBuilder(context, single);
             },
-            separatorBuilder: (BuildContext context, int index) {
-              return SizedBox();
-            },
+            separatorBuilder: separatorBuilder,
             itemCount: state.data.length + (state.isFetching ? 1 : 0),
           );
         }
