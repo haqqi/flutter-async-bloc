4b8606352ee106a32efb22ab7e423c3b5b086b72
haqqi <me@haqqi.net>
2020-04-23 17:32:07 +0700

    🌏 [ADD] provider for single request and unlimited list

---
+++ lib/src/single_request/provider.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+import 'bloc.dart';
+import 'event.dart';
+import 'request.dart';
+
+class SRBlocProvider<R, U extends SRUseCase<R>>
+    extends BlocProvider<SRBloc<R, U>> {
+  SRBlocProvider({
+    @required U Function(BuildContext context) useCase,
+    Widget child,
+    Key key,
+  })  : assert(useCase != null),
+        super(
+          create: (BuildContext context) {
+            return SRBloc<R, U>(
+              useCase: useCase(context),
+            )..add(SRFetchEvent());
+          },
+          key: key,
+        );
+}
---
+++ lib/src/unlimited_list/provider.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+import '../../flutter_async_bloc.dart';
+import 'bloc.dart';
+import 'request.dart';
+
+class ULBlocProvider<M, U extends ULUseCase<M>>
+    extends BlocProvider<ULBloc<M, U>> {
+  ULBlocProvider({
+    @required U Function(BuildContext context) useCase,
+    Widget child,
+    Key key,
+  })  : assert(useCase != null),
+        super(
+          create: (BuildContext context) {
+            return ULBloc(
+              useCase: useCase(context),
+            )..add(ULFetchEvent());
+          },
+          key: key,
+        );
+}
