28288f8e1787196ec352dbeeb9aee16fdcbb3047
haqqi <me@haqqi.net>
2020-04-24 05:16:45 +0700

    😢 [FIN] export all related files

--- lib/flutter_async_bloc.dart
+++ lib/flutter_async_bloc.dart
@@ -1,5 +1,32 @@
 library flutter_async_bloc;
 
-export './src/core/event.dart';
-export './src/core/result.dart';
-export './src/core/state.dart';
+// export package libraries
+//export 'package:bloc/async.dart';
+//export 'package:equatable/equatable.dart';
+//export 'package:flutter_bloc/flutter_bloc.dart';
+//export 'package:nested/nested.dart';
+//export 'package:provider/provider.dart';
+//export 'package:provider/single_child_widget.dart';
+
+export 'src/common/constant.dart';
+export 'src/common/response.dart';
+export 'src/common/widget.dart';
+export 'src/form_submit/bloc.dart';
+export 'src/form_submit/event.dart';
+export 'src/form_submit/provider.dart';
+export 'src/form_submit/request.dart';
+export 'src/form_submit/state.dart';
+export 'src/form_submit/widget.dart';
+export 'src/single_request/bloc.dart';
+export 'src/single_request/event.dart';
+export 'src/single_request/provider.dart';
+export 'src/single_request/request.dart';
+export 'src/single_request/state.dart';
+export 'src/single_request/widget.dart';
+export 'src/unlimited_list/bloc.dart';
+export 'src/unlimited_list/event.dart';
+export 'src/unlimited_list/provider.dart';
+export 'src/unlimited_list/request.dart';
+export 'src/unlimited_list/response.dart';
+export 'src/unlimited_list/state.dart';
+export 'src/unlimited_list/widget.dart';
