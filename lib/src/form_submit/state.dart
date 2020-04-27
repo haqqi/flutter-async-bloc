660e6e38bbe48de88df4553cd0009a8274bda633
haqqi <me@haqqi.net>
2020-04-28 03:09:26 +0700

    👮‍♀️ [ADD] change again the form state

--- lib/src/form_submit/state.dart
+++ lib/src/form_submit/state.dart
@@ -3,24 +3,65 @@ import 'package:flutter/widgets.dart';
 
 import '../common/response.dart';
 
-/// Representation of a form state, with the return of T once the submission has
-/// done. We reuse the [AsyncSnapshot] library from flutter
+/// Representation of a form state, with the return of [R] as [AsyncResponse]
+/// It also contains [F] who hold form data.
 @immutable
-abstract class FormSubmitState<Response> extends Equatable {
+class FormSubmitState<F, R> extends Equatable {
+//  /// Global key to handle the widget form state. Useful for handling validation
+//  /// and submission.
+//  ///
+//  final GlobalKey<FormState> formKey = GlobalKey<FormState>();
+//
+//  /// Validate the form logic using form key or custom form validation
+//  ///
+//  @mustCallSuper
+//  bool validate() {
+//    // if form key is attached, and validation failed, just return false
+//    if (formKey.currentState.mounted && !formKey.currentState.validate()) {
+//      return false;
+//    }
+//
+//    // save the state
+//    formKey.currentState?.save();
+//
+//    // validate the form
+//    return true;
+//  }
+
+  /// Initial form data
+  final F formData;
+
   /// response once done
-  final AsyncResponse<Response> response;
+  final AsyncResponse<R> response;
 
   /// flag for sending
   final bool isSending;
 
-  FormSubmitState(
-    this.response,
-    this.isSending,
-  );
+  FormSubmitState._({
+    this.formData,
+    this.response = null,
+    this.isSending = false,
+  }) : assert(formData != null);
 
-  /// Self return mark sending. Must change the isSending to true.
-  FormSubmitState<Response> markSending();
+  FormSubmitState.init({
+    @required F formData,
+  }) : this._(
+          formData: formData,
+          isSending: false,
+        );
 
-  /// Self return mark success. Must change the isSending to false.
-  FormSubmitState<Response> markDone(AsyncResponse<Response> response);
+  FormSubmitState<F, R> markSending() {
+    return FormSubmitState<F, R>._(
+      formData: formData,
+      isSending: true,
+      response: null,
+    );
+  }
+
+  @override
+  List<Object> get props => [
+        response,
+        isSending,
+        formData,
+      ];
 }
