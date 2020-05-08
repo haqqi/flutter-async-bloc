67d87889f04d5d018ad642b133e4b78c66e4c7a6
haqqi <me@haqqi.net>
2020-05-09 03:32:56 +0700

    👤 [ADD] list usecase

---
+++ lib/src/list/usecase.dart
@@ -0,0 +1,16 @@
+import 'package:meta/meta.dart';
+
+import '../common/response.dart';
+import 'request.dart';
+import 'response.dart';
+
+abstract class AsyncListUseCase<F extends AsyncListRequest,
+    R extends AsyncListResponse<dynamic>> {
+  final String useCaseId;
+
+  AsyncListUseCase.mustCall({
+    @required this.useCaseId,
+  }) : assert(useCaseId != null);
+
+  Future<AsyncResponse<R>> fetch(F request);
+}
